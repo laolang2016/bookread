eaa9e9241693ebd44e7a35a236ac985b61fc2f28
laolang2016 <1164891473@qq.com>
2026-04-26 18:27:53 +0800

    ch03: 服务启动与应用访问

--- how-tomcat-works/ch03/src/main/java/com/laolang/lightserver/startup/Bootstrap.java
+++ how-tomcat-works/ch03/src/main/java/com/laolang/lightserver/startup/Bootstrap.java
@@ -2,6 +2,14 @@ package com.laolang.lightserver.startup;
 
 import com.laolang.lightserver.connector.http.HttpConnector;
 
+/**
+ * 启动后访问:
+ * <ul>
+ *     <li><code>servlet</code>: <a href="http://localhost:8092/servlet/ExampleServlet">http://localhost:8092/servlet/ExampleServlet</a> </li>
+ *     <li>静态资源: <a href="http://localhost:8092/static/index.html">http://localhost:8092/static/index.html</a> </li>
+ * </ul>
+ * 如果使用 <code>idea</code> 启动, 则需要配置 <code>Working Directory</code> 为项目 <code>pom.xml</code> 所在目录
+ */
 public final class Bootstrap {
 
     public static void main(String[] args) {
